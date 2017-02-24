ab20eb2ff68e5b0f57c6e96f5d896e8f689a5c0e
jverbosk <jverbosk@gmail.com>
2017-02-23 20:15:42 -0500

    rewrote existing tests to use symbols instead of strings

--- test_coin_changer_r.rb
+++ test_coin_changer_r.rb
@@ -12,19 +12,19 @@ class TestCoinChangerRedux < Minitest::Test
   def test_2_return_1_penny_for_1
     amount = '1'
     results = make_change(amount)
-    assert_equal({"penny" => 1}, results)
+    assert_equal({penny: 1}, results)
   end
 
   def test_3_return_2_pennies_for_2
     amount = '2'
     results = make_change(amount)
-    assert_equal({"penny" => 2}, results)
+    assert_equal({penny: 2}, results)
   end
 
   def test_4_return_1_nickel_for_5
     amount = '5'
     results = make_change(amount)
-    assert_equal({"nickel" => 5}, results)
+    assert_equal({nickel: 5}, results)
   end
 
 end
