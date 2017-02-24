f6f41d260aba2cc73def41c994f3e78cd1fbe114
jverbosk <jverbosk@gmail.com>
2017-02-23 22:55:41 -0500

    added assertions for form on default path

--- tests/tests_app.rb
+++ tests/tests_app.rb
@@ -12,7 +12,7 @@ class TestApp < Minitest::Test
   def test_get_entry_page
     get '/'
     assert(last_response.ok?)
-    # assert(last_response.body.include?('<form method="post" action="name">'))
-    # assert(last_response.body.include?('<input type="text" name="user_name">'))
+    assert(last_response.body.include?('<form method="post" action="amount">'))
+    assert(last_response.body.include?('<input type="text" name="number">'))
   end
 end
