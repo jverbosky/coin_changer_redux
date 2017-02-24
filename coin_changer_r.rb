f53577edfeb634a2f32bbbf8a87abbcbf8ceef6d
jverbosk <jverbosk@gmail.com>
2017-02-23 22:40:21 -0500

    condensed .each loop further

--- coin_changer_r.rb
+++ coin_changer_r.rb
@@ -2,11 +2,6 @@ def make_change(amount)
   change = {}
   coins = {quarter: 25, dime: 10, nickel: 5, penny: 1}
   total = amount.to_i
-  coins.each do |type, value|
-    if total >= value
-      change[type] = total/coins[type]
-      total -= total/coins[type] * coins[type]
-    end
-  end
+  coins.each { |type, value| (change[type] = total/coins[type]; total -= total/coins[type] * coins[type]) if total >= value }
   return change
 end
