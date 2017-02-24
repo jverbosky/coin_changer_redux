399def274cd5da59662df03fcd0a156adec86182
jverbosk <jverbosk@gmail.com>
2017-02-23 23:28:45 -0500

    added prompt and form for entering another number to results.erb, all assertions passing

--- tests/tests_app.rb
+++ tests/tests_app.rb
@@ -21,7 +21,7 @@ class TestApp < Minitest::Test
     post '/amount', number: '93'
     assert(last_response.ok?)
     assert(last_response.body.include?("{:quarter=>3, :dime=>1, :nickel=>1, :penny=>3}"))
-    assert(last_response.body.include?('Please enter a number to make change:'))
+    assert(last_response.body.include?('Please enter another number to make more change:'))
     assert(last_response.body.include?('<form method="post" action="amount">'))
     assert(last_response.body.include?('<input type="text" name="number">'))
   end
