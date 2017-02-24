8168ac985445287f58efbbc96882e5f3eceb3903
jverbosk <jverbosk@gmail.com>
2017-02-23 20:37:57 -0500

    wrote logic to pass sixth test and changed quotes

--- coin_changer_r.rb
+++ coin_changer_r.rb
@@ -1,10 +1,11 @@
 def make_change(amount)
   change = {}
   case amount
-    when "6" then [change[:nickel] = 1, change[:penny] = 1]
-    when "5" then change[:nickel] = 1
-    when "2" then change[:penny] = 2
-    when "1" then change[:penny] = 1
+    when '10' then change[:dime] = 1
+    when '6' then [change[:nickel] = 1, change[:penny] = 1]
+    when '5' then change[:nickel] = 1
+    when '2' then change[:penny] = 2
+    when '1' then change[:penny] = 1
   end
   return change
 end
