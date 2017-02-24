3aceeca2cd02dd65c3fd22d6a9d329ccc11ba88b
jverbosk <jverbosk@gmail.com>
2017-02-23 20:16:56 -0500

    updated program to use symbols instead of strings, passing all tests

--- coin_changer_r.rb
+++ coin_changer_r.rb
@@ -1,10 +1,10 @@
 def make_change(amount)
   if amount == "5"
-    return {"nickel" => 5}
+    return {nickel: 5}
   elsif amount == "2"
-    return {"penny" => 2}
+    return {penny: 2}
   elsif amount == "1"
-    return {"penny" => 1}
+    return {penny: 1}
   else
     return {}
   end
