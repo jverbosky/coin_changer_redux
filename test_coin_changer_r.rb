ed99bda12442e8ca13b325e57ff9dfc324b620fb
jverbosk <jverbosk@gmail.com>
2017-02-23 20:02:01 -0500

    created first test - return {} for 0

--- test_coin_changer_r.rb
+++ test_coin_changer_r.rb
@@ -3,10 +3,10 @@ require_relative "coin_changer.rb"
 
 class TestCoinChangerRedux < Minitest::Test
 
-  def test_1_
-    amount = 
-    results = 
-    assert_equal(, results)
+  def test_1_return_empty_array_for_0
+    amount = '0'
+    results = make_change(amount)
+    assert_equal({}, results)
   end
 
 end
