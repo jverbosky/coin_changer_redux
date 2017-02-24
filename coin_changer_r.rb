5d4b19f3b40532bf170e85d02be38eece6fb90ba
jverbosk <jverbosk@gmail.com>
2017-02-23 20:19:25 -0500

    added hash and changed results to populate hash, passing all tests

--- coin_changer_r.rb
+++ coin_changer_r.rb
@@ -1,11 +1,11 @@
 def make_change(amount)
+  change = {}
   if amount == "5"
-    return {nickel: 5}
+    change[:nickel] = 5
   elsif amount == "2"
-    return {penny: 2}
+    change[:penny] = 2
   elsif amount == "1"
-    return {penny: 1}
-  else
-    return {}
+    change[:penny] = 1
   end
+  return change
 end
