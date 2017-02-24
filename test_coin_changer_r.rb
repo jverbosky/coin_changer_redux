8ab87bef2c4e66d282f01f80f786480e563b4658
jverbosk <jverbosk@gmail.com>
2017-02-23 20:00:17 -0500

    created test template

---
+++ test_coin_changer_r.rb
@@ -0,0 +1,12 @@
+require "minitest/autorun"
+require_relative "coin_changer.rb"
+
+class TestCoinChangerRedux < Minitest::Test
+
+  def test_1_
+    amount = 
+    results = 
+    assert_equal(, results)
+  end
+
+end
