b869cac47d244929ef193b401acba6bcfbedb19d
jverbosk <jverbosk@gmail.com>
2017-02-23 22:32:38 -0500

    converted if statements into .each loop

--- coin_changer_r.rb
+++ coin_changer_r.rb
@@ -2,29 +2,39 @@ def make_change(amount)
   change = {}
   coins = {quarter: 25, dime: 10, nickel: 5, penny: 1}
   total = amount.to_i
-  if total >= 25
+
+  coins.each do |type, value|
     count = 0
-    count = total/coins[:quarter]
-    change[:quarter] = count
-    total -= count * coins[:quarter]
+    if total >= value
+      count = total/coins[type]
+      change[type] = count
+      total -= count * coins[type]
+    end
   end
-  if total >= 10
-    count = 0
-    count = total/coins[:dime]
-    change[:dime] = count
-    total -= count * coins[:dime]
-  end
-  if total >= 5
-    count = 0
-    count = total/coins[:nickel]
-    change[:nickel] = count
-    total -= count * coins[:nickel]
-  end
-  if total >= 1
-    count = 0
-    count = total/coins[:penny]
-    change[:penny] = count
-    total -= count * coins[:penny]
-  end
+
+  # if total >= 25
+  #   count = 0
+  #   count = total/coins[:quarter]
+  #   change[:quarter] = count
+  #   total -= count * coins[:quarter]
+  # end
+  # if total >= 10
+  #   count = 0
+  #   count = total/coins[:dime]
+  #   change[:dime] = count
+  #   total -= count * coins[:dime]
+  # end
+  # if total >= 5
+  #   count = 0
+  #   count = total/coins[:nickel]
+  #   change[:nickel] = count
+  #   total -= count * coins[:nickel]
+  # end
+  # if total >= 1
+  #   count = 0
+  #   count = total/coins[:penny]
+  #   change[:penny] = count
+  #   total -= count * coins[:penny]
+  # end
   return change
 end
